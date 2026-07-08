2ee29d022415e7eda8e37ab51e9552a5ec5aca28
atsurinBeatrice <atsurin.ange@icloud.com>
2026-07-08 15:24:15 +0900

    Updated some modules

---
+++ test2.R
@@ -0,0 +1,19 @@
+#log_info("Script Starts!", action="start")
+message("[AEGIS Logging]Script Starts!")
+
+print(paste0("/*** Current Project Location: ", renv::project()))
+print(paste0("/*** Library Paths: ",.libPaths()))
+
+#log_info("Processing...", action="in_process")
+message("[AEGIS Logging]Processing...")
+
+print(paste0("/*** Current Working Location: ", getwd()))
+print("/*** Other Info:")
+print(loadedNamespaces())
+print(sessionInfo())
+
+message("[AEGIS Logging]Loading car package with relevant recommended R packages...")
+library(car)
+
+#log_info("Script Ends!", action="end")
+message("[AEGIS Logging]Script Ends!")
